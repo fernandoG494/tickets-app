7bf1fba938f29f8c259fe850f5edf1c2303178e7
fernandoG494 <darkogarcia3@gmail.com>
2024-11-12 10:27:14 -0600

    login view implemented

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    FloatLabelModule,
+    FormsModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    PasswordModule,
+    ButtonModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
   loginForm: FormGroup;
@@ -14,13 +33,13 @@ export class LoginComponent {
   constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
-  };
+  }
 
   onSubmit() {
     if (this.loginForm.valid) {
       console.log(this.loginForm.value);
-    };
-  };
-};
+    }
+  }
+}
